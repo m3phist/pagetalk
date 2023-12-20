41126c028d73e30835367cd1409d11b83ad98c40
Mephisto Aliff <aliff.ibr@icloud.com>
2023-12-20 15:09:27 +0800

    billing url

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -59,7 +59,7 @@ export const appRouter = router({
   createStripeSession: privateProcedure.mutation(async ({ ctx }) => {
     const { userId } = ctx;
 
-    const billingUrl = absoluteUrl('/dashboard/billing');
+    const billingUrl = `${process.env.NEXT_PUBLIC_APP_URL}/dashboard/billing`;
 
     if (!userId) throw new TRPCError({ code: 'UNAUTHORIZED' });
 
